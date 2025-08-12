79549e0eed196deae3847d83f555502d7bfc531d
H7H5 <Serdyk87@ukr.net>
2025-08-12 14:26:33 +0300

    4

--- Assets/Scripts/AttackController.cs
+++ Assets/Scripts/AttackController.cs
@@ -6,6 +6,11 @@ public class AttackController : MonoBehaviour
 {
     public Transform targetToAttack;
 
+    public Material idleStateMaterial;
+    public Material followStateMaterial;
+    public Material attackStateMaterial;
+    public int unitDamage;
+
     private void OnTriggerEnter(Collider other)
     {
         if(other.CompareTag("Enemy") && targetToAttack == null)
@@ -20,6 +25,33 @@ public class AttackController : MonoBehaviour
         {
             targetToAttack = null;
         }
+    }
+
+    public void SetIdleMaterial()
+    {
+        GetComponent<Renderer>().material = idleStateMaterial;
+    }
+
+    public void SetFollowMaterial()
+    {
+        GetComponent<Renderer>().material = followStateMaterial;
+    }
+
+    public void SetAttackMaterial()
+    {
+        GetComponent<Renderer>().material = attackStateMaterial;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, 10f * 0.2f);
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, 1f);
+
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, 1.2f);
     }
 
 }
--- Assets/UnitFollowState.cs
+++ Assets/UnitFollowState.cs
@@ -15,6 +15,7 @@ public class UnitFollowState : StateMachineBehaviour
     {
         attackController = animator.transform.GetComponent<AttackController>();
         agent = animator.transform.GetComponent<NavMeshAgent>();
+        attackController.SetFollowMaterial();
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
@@ -30,12 +31,12 @@ public class UnitFollowState : StateMachineBehaviour
             {
                 agent.SetDestination(attackController.targetToAttack.position);
                 animator.transform.LookAt(attackController.targetToAttack);
-                //float distanceFromTarget = Vector3.Distance(attackController.targetToAttack.position, animator.transform.position);
-                //if (distanceFromTarget < attackingDistance)
-                //{
-                //    agent.SetDestination(animator.transform.position);     
-                //    animator.SetBool("isAttacking", true);
-                //}
+                float distanceFromTarget = Vector3.Distance(attackController.targetToAttack.position, animator.transform.position);
+                if (distanceFromTarget < attackingDistance)
+                {
+                    agent.SetDestination(animator.transform.position);
+                    animator.SetBool("isAttacking", true);
+                }
             }
         }
     }
